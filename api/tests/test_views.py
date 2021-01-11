48da7a0efc514cb57558216e58528dd405197450
Pat Allen <prallen90@gmail.com>
2021-01-10 21:01:40 -0500

    Add high-level view tests

---
+++ api/tests/test_views.py
@@ -0,0 +1,46 @@
+from unittest import mock
+
+from django.test import SimpleTestCase
+
+from api.tests.mocks import (
+    mocked_requests_get_internal_error,
+    mocked_requests_get_no_content,
+    mocked_requests_get,
+)
+
+
+class CheckSepticViewTests(SimpleTestCase):
+    def test_missing_params_error_response(self):
+        response = self.client.get("/check-septic?address=10 Marley Way")
+        self.assertEqual(response.status_code, 400)
+        self.assertDictEqual(
+            response.json(),
+            {"message": "missing required arguments", "data": ["zipcode"]},
+        )
+
+        response = self.client.get("/check-septic")
+        self.assertEqual(response.status_code, 400)
+        self.assertDictEqual(
+            response.json(),
+            {"message": "missing required arguments", "data": ["address", "zipcode"]},
+        )
+
+    def test_extra_params_error_response(self):
+        response = self.client.get(
+            "/check-septic?address=10 Marley Way&zipcode=00000&pool=yes"
+        )
+        self.assertEqual(response.status_code, 400)
+        self.assertDictEqual(
+            response.json(),
+            {"message": "invalid arguments", "data": ["pool"]},
+        )
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get_no_content)
+    def test_external_no_property_found(self, _mock_get):
+        response = self.client.get("/check-septic?address=10 Marley Way&zipcode=00000")
+        self.assertEqual(response.status_code, 204)
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get_no_content)
+    def test_external_no_property_found(self, _mock_get):
+        response = self.client.get("/check-septic?address=10 Marley Way&zipcode=00000")
+        self.assertEqual(response.status_code, 204)
