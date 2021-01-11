353469f0e6ec97f32938b967c475510095f1d9e3
Pat Allen <prallen90@gmail.com>
2021-01-10 23:50:33 -0500

    Add success and internal error HouseCanaryApi tests

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -35,6 +35,12 @@ class CheckSepticViewTests(SimpleTestCase):
             {"message": "invalid arguments", "data": ["pool"]},
         )
 
+    @mock.patch("requests.get", side_effect=mocked_requests_get)
+    def test_external_has_septic(self, _mock_get):
+        response = self.client.get("/check-septic?address=10 Marley Way&zipcode=00000")
+        self.assertEqual(response.status_code, 200)
+        self.assertTrue(response.json()["result"])
+
     @mock.patch("requests.get", side_effect=mocked_requests_get_no_content)
     def test_external_no_property_found(self, _mock_get):
         response = self.client.get("/check-septic?address=10 Marley Way&zipcode=00000")
@@ -44,3 +50,8 @@ class CheckSepticViewTests(SimpleTestCase):
     def test_external_no_property_found(self, _mock_get):
         response = self.client.get("/check-septic?address=10 Marley Way&zipcode=00000")
         self.assertEqual(response.status_code, 204)
+
+    @mock.patch("requests.get", side_effect=mocked_requests_get_internal_error)
+    def test_external_no_property_found(self, _mock_get):
+        response = self.client.get("/check-septic?address=10 Marley Way&zipcode=00000")
+        self.assertEqual(response.status_code, 500)
