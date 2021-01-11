ae973125f0efaec4044b395fe7383910889f7a26
Patrick Allen <prallen90@gmail.com>
2021-01-11 11:34:36 -0500

    Add documentation on 400 response

--- api/views.py
+++ api/views.py
@@ -19,6 +19,7 @@ class CheckHomeHasSeptic(View):
         ---
         200 -> Request completed successfully and true/false determination was made
         204 -> Request completed successfully; but true/false determination could not be made
+        400 -> Request was improperly formatted or has an invalid set of arguments
         500 -> Request failed due internal error: I.e. Could not connect to external API
 
         Response Formats
